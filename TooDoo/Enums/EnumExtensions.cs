e6ad52020827ecadfb96c0f050f23f7aaf552494
stiandroid <stianen@hotmail.com>
2025-01-19 04:03:27 +0100

    ux-forbedringer

---
+++ TooDoo/Enums/EnumExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace TooDoo.Enums;
+
+public static class EnumExtensions
+{
+    public static string GetDescription(this Enum value)
+    {
+        var fieldInfo = value.GetType().GetField(value.ToString());
+
+        if (fieldInfo != null)
+        {
+            var attribute = fieldInfo.GetCustomAttribute<DescriptionAttribute>();
+            if (attribute != null)
+            {
+                return attribute.Description;
+            }
+        }
+
+        return value.ToString();
+    }
+}
